86e891d4211aa2a2924bdae9fe607215c4ec6e35
DaltonSherwin <daltonsherwin@gmail.com>
2020-01-25 18:08:46 +0000

    problems

--- app/models/skol.rb
+++
@@ -1,4 +0,0 @@
-class Skol < ApplicationRecord
-  validates :player, presence: true, length: { maximum: 140, minimum: 3 }
-  validates :position, presence: true, length: { maximum: 50, minimum: 2 }
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'skol#index'
-  resources :skol
+  root 'players#index'
+  resources :playeres
 end
