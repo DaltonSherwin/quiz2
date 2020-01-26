df7a19691452f0fabb07609b0a8dad19754a6917
DaltonSherwin <daltonsherwin@gmail.com>
2020-01-26 20:39:46 +0000

    SCSS

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_001920) do
+ActiveRecord::Schema.define(version: 2020_01_25_180645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,5 @@ ActiveRecord::Schema.define(version: 2020_01_24_001920) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
